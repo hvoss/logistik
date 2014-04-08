2af65ffedab52410dce219576286c66b589edd64
Henrik Voß <hennevoss@gmail.com>
2014-04-08 15:26:51 +0200

    add logging

--- src/main/java/de/hsbremen/kss/App.java
+++ src/main/java/de/hsbremen/kss/App.java
@@ -1,13 +1,17 @@
 package de.hsbremen.kss;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+	
+	private final static Logger LOG = LoggerFactory.getLogger(App.class);
+	
+    public static void main( String[] args ) {
+    	LOG.info("Hello World!");
     }
 }
