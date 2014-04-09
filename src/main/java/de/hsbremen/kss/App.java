2604eb182c1303673061b8f095d521260928eeed
Henrik Voß <hennevoss@gmail.com>
2014-04-09 22:16:24 +0200

    better distance logging

--- src/main/java/de/hsbremen/kss/App.java
+++ src/main/java/de/hsbremen/kss/App.java
@@ -1,6 +1,9 @@
 package de.hsbremen.kss;
 
 import java.io.File;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,32 +15,42 @@ import de.hsbremen.kss.model.Station;
 
 /**
  * Hello world!
- *
+ * 
  */
 public class App {
-	
+
 	/** logging interface */
 	private final static Logger LOG = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		LOG.info("App started");
+
+		ConfigurationParser confParser = new JAXBConfigurationParserImpl();
+
+		File file = new File("conf.xml");
+
+		Configuration configuration = confParser.parseConfiguration(file);
+
+		LOG.info("got " + configuration.getStations().size() + " stations");
+		LOG.info("got " + configuration.getVehicles().size() + " vehicles");
+		LOG.info("got " + configuration.getOrders().size() + " orders");
+
+		logDistancesBetweenStations(configuration.getStations());
 	
-    public static void main( String[] args ) {
-    	LOG.info("App started");
-    	
-    	ConfigurationParser confParser = new JAXBConfigurationParserImpl();
-    
-    	File file = new File("conf.xml");
-    	
-    	Configuration configuration = confParser.parseConfiguration(file);
-    	
-    	LOG.info("got " + configuration.getStations().size() + " stations");
-    	LOG.info("got " + configuration.getVehicles().size() + " vehicles");
-    	LOG.info("got " + configuration.getOrders().size() + " orders");
-    	
-    	for (Station station : configuration.getStations()) {
-    		for (Station otherStation : configuration.getStations()) {
-    			if (station != otherStation) {
-    				LOG.debug("distance between " + station.getName() + " and " + otherStation.getName() + ": " + Math.round(station.distance(otherStation)) + " km");
-    			}
-    		}
-    	}
-    }
+	}
+	
+	private static void logDistancesBetweenStations(Collection<Station> stations) {
+		Set<Station> processedStations = new HashSet<>();
+		for (Station station : stations) {
+			processedStations.add(station);
+			for (Station otherStation : stations) {
+				if (!processedStations.contains(otherStation)) {
+					LOG.debug("distance between " + station.getName() + " and "
+							+ otherStation.getName() + ": "
+							+ Math.round(station.distance(otherStation))
+							+ " km");
+				}
+			}
+		}
+	}
 }
