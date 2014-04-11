9b6231ca49c122b9fe0927ec25d217918413843d
david <mittelstaedt.david@gmail.com>
2014-04-11 16:49:50 +0200

    Edit SavingsConstruction

--- src/main/java/de/hsbremen/kss/construction/SavingsContruction.java
+++ src/main/java/de/hsbremen/kss/construction/SavingsContruction.java
@@ -1,14 +1,41 @@
 package de.hsbremen.kss.construction;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import de.hsbremen.kss.model.Configuration;
+import de.hsbremen.kss.model.Order;
 import de.hsbremen.kss.model.Plan;
+import de.hsbremen.kss.model.Station;
 
 public class SavingsContruction implements Construction {
 
+	private List<Order> orderList;
+	private double savingsValue;
+	private Order sourceOrder;
+	private Order destinationOrder;
+	private Station depot;
+	
 	@Override
 	public Plan constructPlan(Configuration configuration) {
-		// TODO Auto-generated method stub
+		orderList = new ArrayList<Order>(configuration.getOrders());
+		List<Station> stationList = new ArrayList<Station>(configuration.getStations());
+		depot = stationList.get(3);
+		calculateSavingsValue();
 		return null;
+	}
+	
+	private void calculateSavingsValue(){
+		for (Order order1 : orderList){
+			for (Order order2 : orderList){
+				if (order1.getId() != order2.getId()){
+					double savingsValueTemp = depot.distance(order1.getSource()) + depot.distance(order2.getSource()) - 
+							order1.getSource().distance(order2.getSource());
+					System.out.println(order1.getSource().getName() + " " + order2.getSource().getName() + " Savings: " 
+							+ savingsValue);
+				}
+			}
+		}
 	}
 
 }
