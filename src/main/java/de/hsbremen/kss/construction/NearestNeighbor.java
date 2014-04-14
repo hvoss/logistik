355b05baaf3891ed28cf5b07823e8e0afbed8e29
Oliver Pohling <Oliver Pohling@OliverPohling>
2014-04-14 10:03:08 +0200

    Revert "merge"
    This reverts commit 34cff9e42bbb7cea8899923e4ce20fc7b88088b1.

--- src/main/java/de/hsbremen/kss/construction/NearestNeighbor.java
+++ src/main/java/de/hsbremen/kss/construction/NearestNeighbor.java
@@ -1,22 +1,14 @@
 package de.hsbremen.kss.construction;
 
-import java.util.Collection;
-
 import de.hsbremen.kss.configuration.Configuration;
 import de.hsbremen.kss.model.Plan;
-import de.hsbremen.kss.configuration.Station;
 
 public class NearestNeighbor implements Construction {
-	
-	private Plan nearestNeighborPlan;
-	
-	/** a collection of all stations */
-	private Collection<Station> stations;
 
 	@Override
 	public Plan constructPlan(Configuration configuration) {
-		stations = configuration.getStations();
-		return nearestNeighborPlan;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
