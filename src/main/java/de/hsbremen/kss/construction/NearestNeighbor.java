34cff9e42bbb7cea8899923e4ce20fc7b88088b1
Oliver Pohling <Oliver Pohling@OliverPohling>
2014-04-14 10:03:00 +0200

    merge

--- src/main/java/de/hsbremen/kss/construction/NearestNeighbor.java
+++ src/main/java/de/hsbremen/kss/construction/NearestNeighbor.java
@@ -1,14 +1,22 @@
 package de.hsbremen.kss.construction;
 
+import java.util.Collection;
+
 import de.hsbremen.kss.configuration.Configuration;
 import de.hsbremen.kss.model.Plan;
+import de.hsbremen.kss.configuration.Station;
 
 public class NearestNeighbor implements Construction {
+	
+	private Plan nearestNeighborPlan;
+	
+	/** a collection of all stations */
+	private Collection<Station> stations;
 
 	@Override
 	public Plan constructPlan(Configuration configuration) {
-		// TODO Auto-generated method stub
-		return null;
+		stations = configuration.getStations();
+		return nearestNeighborPlan;
 	}
 
 }
