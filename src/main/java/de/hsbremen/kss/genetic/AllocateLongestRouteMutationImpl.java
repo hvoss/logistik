b72bd2784b26d94cf017cc5eb2e0f1e478bfd499
logistik <logistik@logistik>
2014-06-15 03:13:19 +0200

    Mutationsimplementierung

---
+++ src/main/java/de/hsbremen/kss/genetic/AllocateLongestRouteMutationImpl.java
@@ -0,0 +1,70 @@
+package de.hsbremen.kss.genetic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hsbremen.kss.configuration.Order;
+import de.hsbremen.kss.construction.SweepConstruction;
+import de.hsbremen.kss.model.OrderAction;
+import de.hsbremen.kss.model.OrderLoadAction;
+import de.hsbremen.kss.model.OrderUnloadAction;
+import de.hsbremen.kss.model.Plan;
+import de.hsbremen.kss.model.Tour;
+import de.hsbremen.kss.util.RandomUtils;
+
+public class AllocateLongestRouteMutationImpl implements Mutation {
+
+	private final RandomUtils randomUtils;
+
+	public AllocateLongestRouteMutationImpl(final RandomUtils randomUtils) {
+		this.randomUtils = randomUtils;
+	}
+
+	@Override
+	public Plan mutate(final Plan plan) {
+		final Plan newPlan = new Plan(SweepConstruction.class);
+
+		final List<Tour> tours = new ArrayList<>(plan.getTours());
+		Tour tourToRemove = tours.get(0);
+
+		for (Tour tour : tours) {
+			if (tourToRemove.length() < tour.length()) {
+				tourToRemove = tour;
+			}
+		}
+
+		final List<OrderAction> allocatedActions = tourToRemove.getOrderActions();
+		List<OrderAction> newTourActions;
+		tours.remove(tourToRemove);
+
+		while (!allocatedActions.isEmpty()) {
+
+			Tour tourToManipulate = this.randomUtils.randomElement(tours);
+			newTourActions = tourToManipulate.getOrderActions();
+			Tour newTour = newPlan.newTour(tourToManipulate.getVehicle());
+			tours.remove(tourToManipulate);
+			newTour.leafSourceDepot();
+			newTour.addOtherActions(newTourActions);
+
+			Order orderToMove = allocatedActions.get(0).getOrder();
+
+			this.randomUtils.insertAtRandomPosition(newTourActions,
+					new OrderLoadAction(orderToMove));
+			this.randomUtils.insertAtRandomPosition(newTourActions,
+					new OrderUnloadAction(orderToMove));
+
+			allocatedActions.remove(0);
+
+			newTour.gotoDestinationDepot();
+			tours.add(newTour);
+
+		}
+		
+		for (Tour tour : tours) {
+			newPlan.addTour(tour);
+		}
+
+		return newPlan;
+	}
+
+}
